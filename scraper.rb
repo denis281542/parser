f33b3c4bb99f16323331d001344e12c1c9c40c0f
denis281542 <deniskuimov@mail.ru>
2019-11-22 23:17:04 +0500

    Added review link in scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,3 +1,10 @@
 require 'mechanize'
 require 'date'
 require 'json'
+
+review_links = page.links_with(href: %r{^/reviews/albums/\w+})
+
+review_links = review_links.reject do |link|
+  parent_classes = link.node.parent['class'].split
+  parent_classes.any? { |p| %w[next-container page-number].include?(p) }
+end
